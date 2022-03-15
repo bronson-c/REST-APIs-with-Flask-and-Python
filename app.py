f18faace9d80fb4619dcf64e66ec6e5855afed20
Bronson Chapman <bronson@fpcc.ca>
2022-03-14 19:06:28 -0700

    added basic token error handling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
@@ -28,6 +29,46 @@ def add_claims_to_jwt(identity):
     if identity == 1:
         return{'is_admin': True}
     return{'is_admin': False}
+
+
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_payload):
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verification failed',
+        'error': 'invalid_token'
+    }), 401
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token',
+        'error': 'authorization_required'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def stale_token_callback(jwt_header, jwt_payload):
+    return jsonify({
+        'description': 'The access token is not fresh',
+        'error': 'fresh_token_required'
+    }), 401
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback(jwt_header, jwt_payload):
+    return jsonify({
+        'description': 'The access token ihas been revoked',
+        'error': 'token_revoked'
+    }), 401
 
 
 api.add_resource(Store, '/store/<string:name>')
